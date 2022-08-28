e2bb67f8f7692567da6974f0f175839edf6de1eb
Pat <pbmodonnell@utexas.edu>
2022-08-28 13:39:15 -0700

    first commit

---
+++ app/src/main/java/com/example/pbo225_hw0/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pbo225_hw0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
